5c959b13c9f48186bc09f6dbbe53dea7f6619224
Stephen Williams <smeredithwilliams@gmail.com>
2022-08-13 20:31:12 -0700

    added in prettierrc stuff to help with formatting

--- pages/MainBoard.js
+++ pages/MainBoard.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-
 const MainBoard = () => {
+  return (
+    <div>
+      <h1>Main Board Area</h1>
 
-    return (
-        <h1>
-            Main Board Area
-        </h1>
-    )
-}
+      <p>need a zombies container</p>
+      <p>need a survivor team container</p>
+      <p> lets see this get fixed</p>
+    </div>
+  );
+};
 
 export default MainBoard;
