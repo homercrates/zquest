cb3ac00c157c35b81124eddf6bb23832121f644f
Stephen Williams <smeredithwilliams@gmail.com>
2022-08-14 12:35:36 -0700

    adding mainboard

--- pages/MainBoard.js
+++ pages/MainBoard.js
@@ -3,11 +3,21 @@ import React from "react";
 const MainBoard = () => {
   return (
     <div>
-      <h1>Main Board Area</h1>
+      <h1
+        style={{
+          color: "green",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Main Board Area
+      </h1>
 
       <p>need a zombies container</p>
       <p>need a survivor team container</p>
-      <p> lets see this get fixed</p>
+      <p>survors to be found container hidden maybe</p>
+      <p> items container too</p>
     </div>
   );
 };
