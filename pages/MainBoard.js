55a6daf4387a3337fdeb4ee54b87c5aa62500f1e
Stephen Williams <smeredithwilliams@gmail.com>
2022-08-14 21:00:30 -0700

    cat om way

--- pages/MainBoard.js
+++ pages/MainBoard.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
+const peopleArray = [
+  { name: "One", pic: "picture" },
+  { name: "two", pic: "picture" },
+  { name: "three", pic: "picture" },
+];
+
 const MainBoard = () => {
   return (
     <div>
@@ -23,14 +29,41 @@ const MainBoard = () => {
 
       <div className="container">
         <div className="row">
-          <div className="col-4 col-sm-6" style={{ backgroundColor: "blue" }}>
-            <div>1</div>
+          <div
+            className="col-8"
+            style={{
+              backgroundColor: "blue",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {peopleArray.map((ind) => {
+              return (
+                <div>
+                  <div>{ind.name}</div>
+                  <div>{ind.pic}</div>
+                </div>
+              );
+            })}
           </div>
-          <div className="col-4 col-sm-6" style={{ backgroundColor: "green" }}>
-            <div>2</div>
+        </div>
+
+        <div className="row">
+          <div className="col-2" style={{ backgroundColor: "green" }}>
+            <div>left</div>
           </div>
-          <div className="col-4" style={{ backgroundColor: "red" }}>
-            <div>3</div>
+          <div className="col-4" style={{ backgroundColor: "white" }}>
+            <div>Middle</div>
+          </div>
+          <div className="col-2" style={{ backgroundColor: "green" }}>
+            <div>right</div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-8" style={{ backgroundColor: "red" }}>
+            <div>bottom</div>
           </div>
         </div>
       </div>
